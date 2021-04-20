1e322f05597d6811ffe3c8d40a4e86066896f8a7
Pranav Velleleth <pvelleleth@gmail.com>
2021-04-20 12:17:00 -0400

    Driver Setup

--- Driver.java
+++ Driver.java
@@ -1,1 +1,15 @@
+import javax.swing.JFrame;
 
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Hangman Game");
+		frame.setSize(900, 600);
+		frame.setLocation(900, 600);
+		frame.setDefaultCloseOperation(3);
+		frame.setContentPane( new Panel());
+		frame.setVisible(true);
+	}
+
+}
