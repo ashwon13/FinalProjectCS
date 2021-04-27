8da20f000e1472d1536c81010704381b209d2455
Vasudevan <1555811@fcps.edu>
2021-04-27 17:03:08 -0400

    test

--- Panel.java
+++ Panel.java
@@ -9,23 +9,25 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Panel extends JPanel {
-    public Panel() {
-        setLayout(new BorderLayout()); // Setting layout to BorderLayout
+   public Panel() {
+      setLayout(new BorderLayout()); // Setting layout to BorderLayout
+   
+      String word = getWord();
+   
+      JLabel dashes = new JLabel("-"*word.length);
+   }
 
-        String word = getWord();
-
-        JLabel dashes = new JLabel("-"*word.length);
-    }
-
-    public String getWord() throws FileNotFoundException {
-        String[] words = new String[855];
-	String x="Hello"; //this is a test
-        File file = new File("words.txt");
-        Scanner scan = new Scanner(file);
-        for (int i=0;scan.hasNextLine();i++) {
-            words[i] = scan.next();
-        }
-        scan.close();
-        return words[(int)(Math.random()*855)];
-    }
+   public String getWord() throws FileNotFoundException {
+      String[] words = new String[855];
+      String x="Hello"; //this is a test
+      File file = new File("words.txt");
+      Scanner scan = new Scanner(file);
+      for (int i=0;scan.hasNextLine();i++) {
+         words[i] = scan.next();
+      }
+      scan.close();
+      return words[(int)(Math.random()*855)];
+   }
 }
+
+//test
