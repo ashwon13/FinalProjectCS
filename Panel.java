c67dfc28d402a126ae53fe86819f4dbd2682bfe8
Pranav Velleleth <pvelleleth@gmail.com>
2021-04-27 19:23:17 -0400

    Dashes

--- Panel.java
+++ Panel.java
@@ -9,21 +9,31 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Panel extends JPanel {
+    String word;
     public Panel() {
         setLayout(new BorderLayout()); // Setting layout to BorderLayout
+        
+        try { word = getWord(); }
+        catch (FileNotFoundException e) {System.out.println(2);}
+        
 
-        String word = getWord();
-
-        JLabel dashes = new JLabel("-"*word.length);
+        JLabel dashes = new JLabel(repeat("_ ",word.length()));
+        dashes.setFont(new Font("Serif", Font.PLAIN, 50));
+        dashes.setHorizontalAlignment(JLabel.CENTER);
+        add(dashes, BorderLayout.CENTER);
     }
-
+    private String repeat(String ch, int len) {
+        String fin = "";
+        for (int i=0;i<len;i++)
+            fin = fin+ch;
+        return fin;
+    }
     public String getWord() throws FileNotFoundException {
         String[] words = new String[855];
-	String x="Hello"; //this is a test
         File file = new File("words.txt");
         Scanner scan = new Scanner(file);
         for (int i=0;scan.hasNextLine();i++) {
-            words[i] = scan.next();
+            words[i] = scan.nextLine();
         }
         scan.close();
         return words[(int)(Math.random()*855)];
