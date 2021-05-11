27f44e01a9b334e6d4c757672c01612366069fd8
ashwon13 <57849857+ashwon13@users.noreply.github.com>
2021-05-11 18:58:38 -0400

    added more instructions

--- Driver.java
+++ Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String rules = "Rules/Info:\n    - The objective is to fully guess the word before the hangman is drawn\n    - You have 10 incorrect guesses\n    - Once the stick man is fully drawn, you lose\n    - You can only guess one lowercase letter at a time(hint: start with vowels)\n    - Good Luck!!";
+		String rules = "Rules/Info:\n    - The objective is to fully guess the word before the hangman is drawn\n    - You have 10 incorrect guesses\n    - Once the stick man is fully drawn, you lose\n    - You can only guess one lowercase letter at a time, don't type the same letter twice(hint: start with vowels)\n    - Good Luck!!";
 		JOptionPane.showMessageDialog(null, rules, "Welcome to Hangman!!", 0);
 		JFrame frame = new JFrame("Hangman Game");
 		frame.setSize(900, 600);
